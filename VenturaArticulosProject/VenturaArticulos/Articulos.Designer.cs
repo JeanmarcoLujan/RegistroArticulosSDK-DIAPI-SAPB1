0db0fefae2251295fcb4b4831df385201727635c
JeanmarcoLujan <mark.15@hotmail.es>
2021-09-02 08:55:25 -0500

    proyecto terminado
    se cumplio con lo solicitado

---
+++ VenturaArticulosProject/VenturaArticulos/Articulos.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace VenturaArticulos
+{
+    partial class Articulos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ARTICULO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número de artículo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre del  artículo:";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(222, 177);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(217, 20);
+            this.txtNum.TabIndex = 3;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(222, 211);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(217, 20);
+            this.txtNom.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(232, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(95, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "INFO. CONEXION";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(98, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Usuario";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(101, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Contraseña";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(222, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = user;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(222, 84);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = pass;
+            // 
+            // Articulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Articulos";
+            this.Text = "Articulos";
+            this.Load += new System.EventHandler(this.Articulos_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ VenturaArticulosProject/VenturaArticulos/Articulos.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VenturaArticulos.Connection;
+
+namespace VenturaArticulos
+{
+    public partial class Articulos : Form
+    {
+
+        public SAPbobsCOM.Company oCompany = null;
+        public SAPbobsCOM.Items oItem = null;
+
+        public string user = "manager";
+        public string pass = "1336";
+        public Articulos()
+        {
+           
+            InitializeComponent();
+            AccessToB1Async();
+        }
+
+        private void Articulos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string numero, nombre;
+
+            numero = txtNum.Text.Trim();
+            nombre = txtNom.Text.Trim();
+
+            if(numero.Equals(""))
+
+
+            oItem = null;
+            oItem = (SAPbobsCOM.Items)oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oItems);
+
+            if (string.IsNullOrEmpty(numero))
+            {
+                MessageBox.Show("Por favor, ingrese el número de articulo");
+            }else if (string.IsNullOrEmpty(nombre))
+            {
+                MessageBox.Show("Por favor, ingrese el nombre del articulo");
+            }
+            else
+            {
+                oItem.ItemCode = numero;
+                oItem.ItemName = nombre;
+
+
+
+                var result = oItem.Add();
+                if (result == 0)
+                {
+                    MessageBox.Show("El articulo ha sido creado correctamente");
+                    txtNum.Text = "";
+                    txtNom.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Ha ocurrido un error");
+                }
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AccessToB1Async()
+        {
+
+
+            string strMessage = string.Empty;
+            int iServerType = 0;
+            string sServer, sLicenseSever, sUserBD, sPassBD, sCompanyName, sUserB1, sPassB1;
+            try
+            {
+                /*
+                iServerType = comboBox1.SelectedIndex;
+                sServer = textBox1.Text;
+                sLicenseSever = textBox4.Text;
+                sUserBD = textBox2.Text;
+                sPassBD = textBox3.Text;
+                sCompanyName = textBox5.Text;
+                sUserB1 = textBox6.Text;
+                sPassB1 = textBox7.Text;
+                */
+
+                var SDSD = iServerType;
+                iServerType = 0;
+                sServer = "LUJAN";
+                sLicenseSever = "LUJAN";
+                sUserBD = "sa";
+                sPassBD = "seidor";
+                sCompanyName = "SBO_BONAPHARM";
+                sUserB1 = user;
+                sPassB1 = pass;
+
+
+
+                SBOConnection cn = new SBOConnection();
+                cn.ConnectCompany(iServerType, sServer, sLicenseSever, sUserBD, sPassBD, sUserB1, sPassB1, sCompanyName, out strMessage);
+
+
+                if (cn != null && cn.IsConnected)
+                {
+                    this.oCompany = cn.companyObject;
+                }
+                else
+                {
+                    MessageBox.Show("A ocurrido un error en la conexión, ingrese nuevamente la información");
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+       
+    }
+}
